e03139b0899e57110dc6e18e0d04e310d3ec8a63
raquelhr <raquelhr@users.noreply.github.com>
2015-11-11 16:56:41 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#the following functions are useful whenver the matrices are very large and one needs to avoid spending computing time
 
 ## Write a short comment describing this function
-
+#creates and sets the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -19,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+#this function makes the operation "inverse", ie, computes the inverse of the matrix that has been cached
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()
@@ -28,6 +29,7 @@ cacheSolve <- function(x, ...) {
         return(m)
     }
     data <- x$get()
+    #implementing the solve function, which returns the inverse of a matrix (provided it's a square matrix)
     m <- solve(data, ...)
     x$setinv(m)
     m
